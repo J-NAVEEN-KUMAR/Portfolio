cc3598ea2b90944d3e5802fd1f94f9144882368d
J-NAVEEN-KUMAR <naveen.jeruban@gmail.com>
2022-02-26 17:50:59 +0530

    developed about section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,12 +4,78 @@ import { skills } from "../../data";
 const Skills = () => {
   return (
     <div className="Container" id="skills">
-      {/* <div className="bg">
-      
-      </div> */}
-
       <div className="about">
-        <h2>About</h2>
+        <h2>About Me</h2>
+        <div className="browser">
+          <div className="circles">
+            <div className="circle" style={{ backgroundColor: "red" }}></div>
+            <div className="circle" style={{ backgroundColor: "orange" }}></div>
+            <div className="circle" style={{ backgroundColor: "green" }}></div>
+          </div>
+          <div className="top">
+            <div className="dp">
+              <img src="./assets/dp2.png" alt="" />
+            </div>
+            <div className="info">
+              <h3>Naveen Kumar J</h3>
+              <span>MERN Stack Developer</span>
+              <span>+91 7661071399</span>
+              <span>naveen.jerban@gmail.com</span>
+            </div>
+            <div className="social">
+              <div className="infoItemLink">
+                <a
+                  href="https://www.linkedin.com/in/j-naveen-kumar/"
+                  target="_blank"
+                >
+                  <img src="./assets/linkedin.png" alt="" className="icon" />
+                </a>
+                <span>LinkedIn</span>
+              </div>
+              <div className="infoItemLink">
+                <a href="https://github.com/J-NAVEEN-KUMAR" target="_blank">
+                  <img src="./assets/github.png" alt="" className="icon" />
+                </a>
+                <span>GitHub</span>
+              </div>
+            </div>
+          </div>
+          <div className="bottom">
+            <ul>
+              <div className="bullet">
+                <img src="./assets/list.png" alt="" />
+                <li>An Engineering graduate and Software employee.</li>
+              </div>
+              <div className="bullet">
+                <img src="./assets/list.png" alt="" />
+                <li>Passionate web desinger and web developer.</li>
+              </div>
+              <div className="bullet">
+                <img src="./assets/list.png" alt="" />
+                <li>Always enthusiastic to learn new web technologies.</li>
+              </div>
+              <div className="bullet">
+                <img src="./assets/list.png" alt="" />
+                <li>
+                  Goal is to provide experience of efficient and user-friendly
+                  applications.
+                </li>
+              </div>
+              <div className="bullet">
+                <img src="./assets/list.png" alt="" />
+                <li>Love reading novels.</li>
+              </div>
+              <div className="bullet">
+                <img src="./assets/list.png" alt="" />
+                <li>Enjoys long drives.</li>
+              </div>
+            </ul>
+          </div>
+          <div className="quote">
+            <img src="./assets/quote.gif" alt="" />
+            <span>"Our body needs routine, but the mind doesn't. So, keep exploring."</span>
+          </div>
+        </div>
       </div>
 
       <div className="skills">
