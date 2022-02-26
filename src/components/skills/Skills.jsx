cd94efd365b2f0a9b9ce98fb88129b82b1b483f5
J-NAVEEN-KUMAR <naveen.jeruban@gmail.com>
2022-02-26 13:05:09 +0530

    skills section modified

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,19 +1,39 @@
-import "./skills.scss"
+import "./skills.scss";
+import { skills } from "../../data";
 
 const Skills = () => {
   return (
-    <div className="container" id="skills">
+    <div className="Container" id="skills">
       {/* <div className="bg">
-        
+      
       </div> */}
+
       <div className="about">
-        <h3>About</h3>
+        <h2>About</h2>
       </div>
+
       <div className="skills">
-        <h3>Skills</h3>
+        <h2>My Skill Set</h2>
+        <div className="skillContainer">
+          <div className="anime">
+            <iframe
+              src="https://giphy.com/embed/4bjIKBOWUnVPICCzJc"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div className="skillSection">
+            {skills.map((s) => (
+              <div className="skillItem">
+                <img src={s.icon} alt="" className="icon" />
+                <span className="title">{s.title}</span>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
--- src/data.js
+++ src/data.js
@@ -1,34 +1,97 @@
 //Skills set data
-
-
+export const skills = [
+  {
+    id: "1",
+    icon: "./assets/skills/html5.png",
+    title: "HTML5",
+  },
+  {
+    id: "2",
+    icon: "./assets/skills/css3.png",
+    title: "CSS3",
+  },
+  {
+    id: "3",
+    icon: "./assets/skills/javascript.png",
+    title: "JavaScript",
+  },
+  {
+    id: "4",
+    icon: "./assets/skills/reactjs.png",
+    title: "ReactJs",
+  },
+  {
+    id: "5",
+    icon: "./assets/skills/mongodb.png",
+    title: "MongoDB",
+  },
+  {
+    id: "6",
+    icon: "./assets/skills/nodejs.png",
+    title: "NodeJs",
+  },
+  {
+    id: "7",
+    icon: "./assets/skills/postman.png",
+    title: "Postman API",
+  },
+  {
+    id: "8",
+    icon: "./assets/skills/bootstrap.png",
+    title: "Bootstap",
+  },
+  {
+    id: "9",
+    icon: "./assets/skills/materialui.png",
+    title: "Material UI",
+  },
+  {
+    id: "10",
+    icon: "./assets/skills/sass.png",
+    title: "SASS",
+  },
+  {
+    id: "11",
+    icon: "./assets/skills/vscode.png",
+    title: "VSCode",
+  },
+  {
+    id: "12",
+    icon: "./assets/skills/githubicon.png",
+    title: "GitHub",
+  },
+  {
+    id: "13",
+    icon: "./assets/skills/git.png",
+    title: "Git",
+  },
+];
 
 //Projects data
 const projects = [
   {
-    id:"1",
-    icon:"./assets/smartphone.png",
-    title:"Project 1",
-    desc:"project description",
-    img:"./assets/web1.jpg"
+    id: "1",
+    icon: "./assets/smartphone.png",
+    title: "Project 1",
+    desc: "project description",
+    img: "./assets/web1.jpg",
   },
   {
-    id:"2",
-    icon:"./assets/website.png",
-    title:"Project 2",
-    desc:"project description",
-    img:"./assets/web2.jpg"
+    id: "2",
+    icon: "./assets/website.png",
+    title: "Project 2",
+    desc: "project description",
+    img: "./assets/web2.jpg",
   },
   {
-    id:"3",
-    icon:"./assets/website.png",
-    title:"Project 3",
-    desc:"project description 3",
-    img:"./assets/web3.jpg"
-  }
-]
+    id: "3",
+    icon: "./assets/website.png",
+    title: "Project 3",
+    desc: "project description 3",
+    img: "./assets/web3.jpg",
+  },
+];
 
 export default projects;
 
-
 //Achievements data
-
